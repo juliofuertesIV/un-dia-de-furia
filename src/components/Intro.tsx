3f680ff6e78baae4b8c116c39de154e163df3e2d
Julio Fuertes <juliofuertestarin@gmail.com>
2024-04-02 13:55:21 +0200

    aesthetics

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -5,9 +5,9 @@ export default function Intro() {
         <section className="text-right font-antique text-2xl max-w-2xl mx-auto text-neutral-700 min-h-screen px-8">
             <PoemImage src={ 'intro' }/>
             <div className="pt-8">
-                <h4>No quiero a tu abuela</h4>
+                <h4>«No quiero a tu abuela</h4>
                 <h4>No quiero a mi novia</h4>
-                <h4>Quiero ser peligroso</h4>
+                <h4>Quiero ser peligroso»</h4>
                 <h5>
                     (E<span className="uppercase text-lg">l</span>&nbsp;
                     O<span className="uppercase text-lg">tro</span>&nbsp;
